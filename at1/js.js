a573cfed668396a424cdc6c267ba3eba0e164e8a
Nikolas._.Rodrigues <114264233+Nikolas-Rodrigues@users.noreply.github.com>
2025-04-21 16:56:02 -0300

    Add files via upload

---
+++ at1/js.js
@@ -0,0 +1,55 @@
+let buscar = document.querySelector("#busca");
+let loginn = document.querySelector("#login");
+
+buscar.addEventListener("click", (e) => {
+    alert("A função de pesquisa ainda não foi criada, amanhã pode ser que funcione")
+}
+)
+loginn.addEventListener("click", (e) => {
+    alert("Login não esta disponivel no momento, tente explorar outras funcionalidades enquanto aguarda!")
+}
+)
+
+
+let prod = document.querySelector("#prod");
+
+let prod1 = document.querySelector("#prod1");
+
+let prod2 = document.querySelector("#prod2");
+
+let prod3 = document.querySelector("#prod3");
+
+let prod4 = document.querySelector("#prod4");
+
+let prod5 = document.querySelector("#prod5");
+
+prod.addEventListener("click", (e) => {
+    alert("Produto esgotado, sinto muito!")
+}
+)
+
+prod1.addEventListener("click", (e) => {
+    alert("Produto esgotado, sinto muito!")
+}
+)
+
+prod2.addEventListener("click", (e) => {
+    alert("Produto esgotado, sinto muito!")
+}
+)
+
+prod3.addEventListener("click", (e) => {
+    alert("Produto esgotado, sinto muito!")
+}
+)
+
+prod4.addEventListener("click", (e) => {
+    alert("Produto esgotado, sinto muito!")
+}
+)
+
+prod5.addEventListener("click", (e) => {
+    alert("Produto esgotado, sinto muito!")
+}
+)
+
